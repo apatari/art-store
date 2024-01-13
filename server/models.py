7a6ab2ff86b655fca1397940e7671526d8be43fc
Andy Patari <andy.patari@gmail.com>
2024-01-13 13:55:58 -0500

    add Seller model

--- server/models.py
+++ server/models.py
@@ -1,6 +1,41 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.hybrid import hybrid_property
 
-from config import db
+from config import db, bcrypt
 
 # Models go here!
+
+class Seller(db.Model, SerializerMixin):
+    __tablename__ = 'sellers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String)
+
+    @validates('username')
+    def validate_username(self, key, name):
+        if not name or not 0 < len(name) <= 20:
+            raise ValueError("Name must be 1-20 characters")
+        if name in [seller.username for seller in Seller.query.all()]:
+            raise ValueError("Sorry, that name is not available")
+        
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hashes may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        if len(password) < 4:
+            raise ValueError("Passwords must be 4 or more characters")
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+    
+    def __repr__(self):
+        return f'Seller {self.username}, ID: {self.id}'
