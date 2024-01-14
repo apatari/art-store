2ba371c67e3fe12b18bbb45db47e47c5f85c25eb
Andy Patari <andy.patari@gmail.com>
2024-01-14 13:40:07 -0500

    add login api route

--- server/app.py
+++ server/app.py
@@ -3,20 +3,43 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request, session
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
 # Add your model imports
-from models import Seller
+from models import Seller, Piece
+
 
 
 # Views go here!
 
-@app.route('/')
-def index():
-    return '<h1>Project Server</h1>'
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = Seller.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            response_body = {
+                "id": user.id,
+                "username": user.username,
+            }
+            return response_body, 200
+        else:
+            return {"errors": ["Invalid username and/or password"]}, 401
+
+
+
+api.add_resource(Login, '/api/login')
+
+# @app.route('/')
+# def index():
+#     return '<h1>Project Server</h1>'
 
 
 if __name__ == '__main__':
