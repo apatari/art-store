e64f814d63bbe0b81d07d0ce1a1370faeac9d7f4
Andy Patari <andy.patari@gmail.com>
2024-01-30 09:49:45 -0500

    fetch pieces in body

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import PieceList from "./PieceList";
 
 function Body() {
+
+    const [pieces, setPieces] = useState(null)
+
+    useEffect(() => {
+        fetch('/api/pieces')
+        .then(res => res.json())
+        .then(data => {
+            setPieces(data)
+            console.log(data)
+        })
+    }, [])
+
+
     return (
         <div>
             Start with sold carousel, cards below that
+            <PieceList />
         </div>
     )
 }
---
+++ client/src/components/PieceList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PieceList() {
+    return (
+        <div className="my-3 mx-5 d-flex flex-wrap" >
+            Pieces here
+        </div>
+    )
+}
+
+export default PieceList
