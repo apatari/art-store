47710d53f2129eaf3afa78fa64b5ec43c73ef647
Andy Patari <andy.patari@gmail.com>
2024-02-02 13:23:18 -0500

    add option button group and handler fns

--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
-import { Button } from "react-bootstrap";
+import { Button, ButtonGroup } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Seller() {
 
     const [user, setUser] = useState(null)
     const history = useHistory()
+    const [mode, setMode] = useState("new")
 
     function handleLogoutClick() {
         fetch('/api/logout', {
@@ -30,10 +31,26 @@ function Seller() {
         <Login setUser={setUser}/>
     )
 
+    const handleNewClick = () => {
+        setMode("new")
+    }
+    const handleEditClick = () => {
+        setMode("edit")
+    }
+    const handleDeleteClick = () => {
+        setMode("delete")
+    }
+
     return (
-        <div>
-            <h2>Seller page</h2>
+        <div className="m-3" >
             <Button onClick={handleLogoutClick} >Logout</Button>
+            <h2>Seller page</h2>
+            <ButtonGroup aria-label="Basic example">
+                <Button variant="info" onClick={handleNewClick} >New</Button>
+                <Button variant="light" onClick={handleEditClick}  >Edit</Button>
+                <Button variant="warning" onClick={handleDeleteClick} >Delete</Button>
+            </ButtonGroup>
+            <h3>{mode}</h3>
         </div>
     )
 }
