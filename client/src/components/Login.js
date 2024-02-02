ede8e069fc416d02ef1071f04833e1c5c14dc427
Andy Patari <andy.patari@gmail.com>
2024-02-02 11:00:31 -0500

    basic login and logout for seller

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,20 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Col } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
-function Login() {
+function Login({ setUser }) {
+
+    const [errors, setErrors] = useState([])
+    const history = useHistory()
+
+    const formSchema = yup.object().shape({
+        username: yup.string().required("Please enter a username"),
+        password: yup.string().required("Please enter a password")
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: ""
+        },
+        validationSchema: formSchema,
+        validateOnChange: false,
+        validateOnBlur: false,
+        onSubmit: (values) => {
+            setErrors([])
+            fetch("/api/login", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(values),
+            }).then(r => {
+                if (r.ok) {
+                    r.json().then(user =>setUser(user)) 
+                    history.push('/seller')    
+                } else {
+                    r.json().then(err => setErrors(err.errors))
+                }
+            })
+        }
+    })
+
+
     return (
         <div className="d-flex m-4" >
             <Col lg={4}>
-            <Form  >
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form onSubmit={formik.handleSubmit} >
+                <Form.Group className="mb-3" >
                     <Form.Label>User</Form.Label>
-                    <Form.Control type="email" placeholder="Enter username" />
+                    <Form.Control 
+                        type="username" 
+                        name="username" 
+                        id="username" 
+                        placeholder="Enter username"
+                        value={formik.values.username}
+                        onChange={formik.handleChange} />
                     
                 </Form.Group>
 
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3" >
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control 
+                        type="password" 
+                        name="password" 
+                        id="password" 
+                        placeholder="Password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange} />
                 </Form.Group>
                 
                 <Button variant="primary" type="submit">
--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,9 +1,20 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
+import { Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 function Seller() {
 
     const [user, setUser] = useState(null)
+    const history = useHistory()
+
+    function handleLogoutClick() {
+        fetch('/api/logout', {
+            method: "DELETE"
+        })
+        .then(r => r.json())
+        .then(data => history.push("/"))
+    }
 
     useEffect(() => {
         fetch("/api/check_session")
@@ -16,12 +27,13 @@ function Seller() {
     }, [])
 
     if (!user) return (
-        <Login />
+        <Login setUser={setUser}/>
     )
 
     return (
         <div>
             <h2>Seller page</h2>
+            <Button onClick={handleLogoutClick} >Logout</Button>
         </div>
     )
 }
