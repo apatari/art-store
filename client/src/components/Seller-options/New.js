4352fbde3ccbb9e0bd716a6333f7be7bc4956087
Andy Patari <andy.patari@gmail.com>
2024-02-05 12:27:38 -0500

    formik and yup for new piece form

--- client/src/components/Seller-options/New.js
+++ client/src/components/Seller-options/New.js
@@ -1,23 +1,72 @@
 import React from "react";
-import { Form, Row, Col } from "react-bootstrap";
+import { Form, Row, Col, Button } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as yup from "yup"
 
 function New() {
+
+    const formSchema = yup.object().shape({
+        name: yup.string()
+            .required("Must provide a name")
+            .max(50, "Name must be 50 characters or fewer"),
+        price: yup.number().typeError("Price must be a number")
+            .positive("Price muse be greater than zero")
+            .integer("Price must be an integer")
+            .required("Please enter a price"),
+        description: yup.string().required("Please enter a description"),
+        image_url: yup.string()
+            .required("Please provide an image url")
+        
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            price: 0,
+            description: "",
+            image_url: ""
+
+        },
+        validationSchema: formSchema,
+        validateOnChange: false,
+        validateOnBlur: false,
+        onSubmit: (values) => {console.log(values)}
+    })
+
+
+
     return (
         <div>
             <h3>Add New Piece</h3>
-            <Form>
+            <Form onSubmit={formik.handleSubmit} >
                 <Row>
                     <Col md={9}>
                         <Form.Group className="mb-3" >
                             <Form.Label>Name</Form.Label>
-                            <Form.Control placeholder="Name" />
+                            <Form.Control 
+                                placeholder="Name" 
+                                type="name"
+                                name="name"
+                                id="name"
+                                value={formik.values.name}
+                                onChange={formik.handleChange}
+                            />
                         </Form.Group>
                     </Col>
 
                     <Col md={3}>
                         <Form.Group className="mb-3" >
                             <Form.Label>Price</Form.Label>
-                            <Form.Control type="number" placeholder="Price" min={1} step={1} />
+                            <Form.Control 
+                                type="number" 
+                                placeholder="Price" min={1} 
+                                step={1} 
+                               
+                                name="price"
+                                id="price"
+                                value={formik.values.price}
+                                onChange={formik.handleChange}
+                            />
                         </Form.Group>
                     </Col>
 
@@ -25,13 +74,29 @@ function New() {
                 
                 <Form.Group className="mb-3" >
                     <Form.Label>Description</Form.Label>
-                    <Form.Control as="textarea" rows={3} />
+                    <Form.Control 
+                        as="textarea" 
+                        rows={3} 
+                        
+                        name="description"
+                        id="description"
+                        value={formik.values.description}
+                        onChange={formik.handleChange}
+                    />
                 </Form.Group>
 
                 <Form.Group className="mb-3" >
                             <Form.Label>Image URL</Form.Label>
-                            <Form.Control  placeholder="Paste image address here"/>
+                            <Form.Control  
+                                placeholder="Paste image address here"
+                                
+                                name="image_url"
+                                id="image_url"
+                                value={formik.values.image_url}
+                                onChange={formik.handleChange}
+                            />
                         </Form.Group>
+                <Button type="submit" >Submit</Button>
             </Form>
 
         </div>
