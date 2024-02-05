3079479c590a7b5d706187e4b8b8fdbf07099793
Andy Patari <andy.patari@gmail.com>
2024-02-05 12:16:50 -0500

    add new piece form fields

--- client/src/components/Seller-options/New.js
+++ client/src/components/Seller-options/New.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { Form, Row, Col } from "react-bootstrap";
 
 function New() {
     return (
-        <div>New</div>
+        <div>
+            <h3>Add New Piece</h3>
+            <Form>
+                <Row>
+                    <Col md={9}>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Name</Form.Label>
+                            <Form.Control placeholder="Name" />
+                        </Form.Group>
+                    </Col>
+
+                    <Col md={3}>
+                        <Form.Group className="mb-3" >
+                            <Form.Label>Price</Form.Label>
+                            <Form.Control type="number" placeholder="Price" min={1} step={1} />
+                        </Form.Group>
+                    </Col>
+
+                </Row>
+                
+                <Form.Group className="mb-3" >
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" >
+                            <Form.Label>Image URL</Form.Label>
+                            <Form.Control  placeholder="Paste image address here"/>
+                        </Form.Group>
+            </Form>
+
+        </div>
     )
 }
 
