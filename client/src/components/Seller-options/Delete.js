2eaa41086553cf7f7fb99e3a30d488c37cde92fa
Andy Patari <andy.patari@gmail.com>
2024-02-05 13:24:45 -0500

    delete functional

--- client/src/components/Seller-options/Delete.js
+++ client/src/components/Seller-options/Delete.js
@@ -1,10 +1,16 @@
-import React from "react";
-
+import React, { useState } from "react";
+import DeleteButton from "./DeleteButton";
 
 function Delete({ pieces }) {
+
     return (
         <div>
-            {pieces.map(piece => <p key={piece.id} >{piece.name}</p>)}
+            <h3>Click the title of the piece to delete it</h3>
+            <div className="m-3"  >
+                {pieces.map(piece => <DeleteButton key={piece.id} piece = {piece} />)}
+            </div>
+
+            
         </div>
     )
 }
---
+++ client/src/components/Seller-options/DeleteButton.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+
+function DeleteButton({ piece }) {
+
+    const history = useHistory()
+
+    const [showModal, setShowModal] = useState(false)
+    
+    const handleShow = () => setShowModal(true)
+    const handleClose = () => setShowModal(false)
+
+
+
+        
+        const handleDeleteConfirm = () => {
+            
+            fetch(`/api/pieces/${piece.id}`, {
+                method: "DELETE"
+            })
+            .then(r => {
+                if (r.ok) {
+                    
+                    history.push('/')
+                } else {
+                    alert("Error: delete unsuccessful")
+                    handleClose()
+                }
+            })
+        }
+
+
+
+    return (
+        <div className="m-2" >
+            <Button className="btn-danger rounded"  onClick={handleShow} >{piece.name}</Button>
+            <Modal centered  show={showModal} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Confirm Delete Piece: {piece.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="text-dark fs-5" >This will delete the piece permanently</Modal.Body>
+                <Modal.Footer>
+                <Button className="btn me-auto" onClick={handleClose}>
+                    Cancel
+                </Button>
+                <Button className="btn-info " onClick={handleDeleteConfirm}>
+                    Delete it
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+        
+    )
+}
+
+export default DeleteButton
