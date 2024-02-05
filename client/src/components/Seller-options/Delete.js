ea79d2d167c0a9253ccd440ddbdf68719584013b
Andy Patari <andy.patari@gmail.com>
2024-02-05 12:06:18 -0500

    create seller views and routing

---
+++ client/src/components/Seller-options/Delete.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Delete() {
+    return (
+        <div>Del</div>
+    )
+}
+
+export default Delete
--- client/src/components/Seller.js
+++ client/src/components/Seller.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import Login from "./Login";
-import { Button, ButtonGroup } from "react-bootstrap";
+import { Button, ButtonGroup, Col, Container } from "react-bootstrap";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import New from "./Seller-options/New";
+import Edit from "./Seller-options/Edit";
+import Delete from "./Seller-options/Delete";
 
 function Seller() {
 
@@ -44,13 +47,20 @@ function Seller() {
     return (
         <div className="m-3" >
             <Button onClick={handleLogoutClick} >Logout</Button>
-            <h2>Seller page</h2>
+            <h2 className="m-3" >Seller page</h2>
             <ButtonGroup aria-label="Basic example">
                 <Button variant="info" onClick={handleNewClick} >New</Button>
                 <Button variant="light" onClick={handleEditClick}  >Edit</Button>
                 <Button variant="warning" onClick={handleDeleteClick} >Delete</Button>
             </ButtonGroup>
-            <h3>{mode}</h3>
+        
+        <Col className="m-3 p-3 bg-light rounded" lg={8} >
+            {(mode === "new")? <New/> : ""}
+            {(mode === "edit")? <Edit/> : ""}
+            {(mode === "delete")? <Delete/> : ""}
+        </Col>
+        
+
         </div>
     )
 }
