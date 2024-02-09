8befedf0ad64fcf0ee09a398c0e8f08d23e933c9
Andy Patari <andy.patari@gmail.com>
2024-02-09 10:28:59 -0500

    set piece using params

--- client/src/components/PieceDetail.js
+++ client/src/components/PieceDetail.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 function PieceDetail() {
 
     const { piece_id } = useParams()
+    const [piece, setPiece] = useState(null)
 
+    useEffect(() =>{
+        fetch(`/api/pieces/${piece_id}`)
+        .then(res => {if (res.ok)
+            {res.json()
+            .then(data => setPiece(data))}})
+        
+    }, [])
+
+    
     return (
         <div>
             <h2>view for piece {piece_id} </h2>
+            {piece && <h4>There is a piece here</h4>}
         </div>
     )
 }
