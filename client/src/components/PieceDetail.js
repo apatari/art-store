bbf1eecde9fd36fd860a2515ec1dd2c0a552bfec
Andy Patari <andy.patari@gmail.com>
2024-02-09 10:32:52 -0500

    handle incorrect piece number

--- client/src/components/PieceDetail.js
+++ client/src/components/PieceDetail.js
@@ -5,18 +5,26 @@ function PieceDetail() {
 
     const { piece_id } = useParams()
     const [piece, setPiece] = useState(null)
+    const [notFound, setNotFound] = useState(false)
 
     useEffect(() =>{
         fetch(`/api/pieces/${piece_id}`)
         .then(res => {if (res.ok)
             {res.json()
-            .then(data => setPiece(data))}})
+            .then(data => setPiece(data))} else{setNotFound(true)} })
         
     }, [])
 
-    
+    // use state to check if the piece wasn't found, then show an error page with an else clause above
+
+    if (notFound) {
+        return (
+            <h3>Piece not found!</h3>
+        )
+    }
+
     return (
-        <div>
+        <div className="m-4" >
             <h2>view for piece {piece_id} </h2>
             {piece && <h4>There is a piece here</h4>}
         </div>
