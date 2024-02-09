b41ed136d0e0a50a2d4ffd8e9857ff89a9f05b8b
Andy Patari <andy.patari@gmail.com>
2024-02-09 14:48:51 -0500

    style piece info

--- client/src/components/PieceDetail.js
+++ client/src/components/PieceDetail.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { Row, Col } from "react-bootstrap";
 
 function PieceDetail() {
 
@@ -22,15 +23,24 @@ function PieceDetail() {
             <h3 className="m-4" >Oops, looks like that piece doesn't exist...</h3>
         )
     }
-
+    if (piece){
     return (
-        <div className="m-4 bg-light rounded p-3" >
-            <h2>{piece.name} </h2>
-            <img src={`/api/pics/${piece.image_url}`} alt="Piece" style={{width: '40%'}} />
-            <h5>{piece.description}</h5>
-            <h3>{piece.price}</h3>
+        <div className="m-3" >
+            <Row  className="m-4  bg-light rounded p-3" >
+                <Col lg={6} className="m-3"  >
+                    <h2>{piece.name} </h2>
+                    <h5>{piece.description}</h5>
+                    <h3>{piece.price}</h3>
+                </Col>
+                <Col className="d-flex m-3" >
+                    <img className="mx-auto"  src={`/api/pics/${piece.image_url}`} alt="Piece" style={{width: '70%'}} />
+                </Col>
+            </Row>
+            
+            
+            
         </div>
-    )
+    )}
 }
 
 export default PieceDetail
