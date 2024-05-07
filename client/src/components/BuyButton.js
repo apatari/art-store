2c8f4786f6e382894b4e30a705ca6f32b084810d
Andy Patari <andy.patari@gmail.com>
2024-05-07 09:19:49 -0400

    add payment modal

--- client/src/components/BuyButton.js
+++ client/src/components/BuyButton.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Modal } from "react-bootstrap";
 
 function BuyButton() {
+
+        const [show, setShow] = useState(false)
+        const handleClose = () => setShow(false);
+        const handleShow = () => setShow(true);
+
+
+
     return(
-        <Button>Buy this piece</Button>
+        <div>
+            <Button onClick={handleShow} >Buy this piece</Button>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Enter Payment Information</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Stripe stuff and a form element here</Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                <Button variant="primary" onClick={handleClose}>
+                    Buy(PH)
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
     )
         
     
