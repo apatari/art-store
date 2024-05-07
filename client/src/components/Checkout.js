94c28e298bb2985e4ed0a16bb274021fb19c7c26
Andy Patari <andy.patari@gmail.com>
2024-05-07 12:34:54 -0400

    send piece info when creating payment intent

--- client/src/components/Checkout.js
+++ client/src/components/Checkout.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState }  from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import CheckoutForm from "./CheckoutForm";
+import { Row, Col, Button } from "react-bootstrap";
 // stripe
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
@@ -12,6 +13,7 @@ function Checkout() {
     const { piece_id } = useParams()
     const [piece, setPiece] = useState(null)
     const [notFound, setNotFound] = useState(false)
+    const [clientSecret, setClientSecret] = useState("")
     
 
     useEffect(() =>{
@@ -19,8 +21,31 @@ function Checkout() {
         .then(res => {if (res.ok)
             {res.json()
             .then(data => setPiece(data))} else{setNotFound(true)} })
+            
+
+        
         
     }, [piece_id])
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("/api/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "piece": piece }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret))
+
+    }, [piece])
+
+    const appearance = {
+        theme: 'stripe',
+      };
+      const options = {
+        clientSecret,
+        appearance,
+      };
 
 
     if (notFound) {
@@ -30,7 +55,40 @@ function Checkout() {
     }
     if (piece){
     return (
-        <CheckoutForm piece={piece} />
+        <div>
+           {clientSecret && (
+            <Elements options={options} stripe={stripePromise}>
+              <CheckoutForm />
+            </Elements>
+          )}
+         
+            <div className="m-3" >
+                <Row  className="m-4  bg-light rounded p-3" >
+                    <Col lg={6} className="m-3"  >
+                        <h2 className="my-3" >Checkout: {piece.name} </h2>
+                        <img className="mx-auto" src={`/api/pics/${piece.image_url}`} alt="Piece" style={{width: '20%', cursor:'pointer'}} />
+                        <h3 className="my-3" >${piece.price}</h3> 
+                    </Col>
+                    
+
+                <Row>
+                    <Col sm={2} >
+                        <Button>Purchase</Button>
+                    </Col>
+                    <Col>
+                        <Button>Cancel</Button>
+                    </Col>
+                </Row>
+                </Row>
+                                        
+                
+            </div>          
+        
+          
+          
+        </div>
+        
+        
     )}
 }
 
