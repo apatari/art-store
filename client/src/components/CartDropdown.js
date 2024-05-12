2cebada9079b3c59bee66a7b4647790a2c0c840b
Andy Patari <andy.patari@gmail.com>
2024-05-12 19:46:24 -0400

    add placeholder offcanvas for cart

--- client/src/components/CartDropdown.js
+++ client/src/components/CartDropdown.js
@@ -1,14 +1,14 @@
 import React, { useContext, useEffect } from "react";
 import { CartContext } from "./App";
 
-export default function CartDropdown() {
+export default function CartDropdown({ handleOpen }) {
 
     const [cart] = useContext(CartContext)
     
 
     return(
-        <div className="d-flex m-3 ">
-            <p className="my-auto fs-5 bg-info bg-opacity-25 p-2 rounded ">Shopping Cart {(cart.length === 0)? "" : "(" + (cart.length)  + ")"} </p>
+        <div  className="d-flex m-3 ">
+            <p style={{cursor: "pointer"}} onClick={handleOpen} className="my-auto fs-5 bg-info bg-opacity-25 p-2 rounded ">Shopping Cart {(cart.length === 0)? "" : "(" + (cart.length)  + ")"} </p>
         </div>
     )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,33 +1,52 @@
-import React from "react";
-import { Nav, Container, Navbar} from "react-bootstrap"
+import React, { useState } from "react";
+import { Nav, Container, Navbar, Offcanvas} from "react-bootstrap"
 import "./header.css"
 import CartTest from "./CartTest";
 import CartDropdown from "./CartDropdown";
 
 
 function Header() {
-    
+
+    const [show, setShow] = useState(false)
+    const handleClose = () => setShow(false)
+    const handleOpen = () => setShow(true)
+
     return (
-        <Navbar expand="lg" className="  ">
-            <Container>
-                <Navbar.Brand href="/" className="fs-2 text-secondary" style={{fontFamily: "revert" }} >
-                    <img src="/ML.png" alt="ML logo" style={{height: "100px"}} />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                
-                <Nav className="ms-auto fs-3">
-                    <Nav.Link href="/" className="mx-2" >Weavings</Nav.Link>
-                    {/* <Nav.Link href="/about" className="mx-2" >About</Nav.Link> */}
-                    <Nav.Link href="/contact" className="mx-2" >Contact</Nav.Link>
+        <div>
+            <Navbar expand="lg" className="  ">
+                <Container>
+                    <Navbar.Brand href="/" className="fs-2 text-secondary" style={{fontFamily: "revert" }} >
+                        <img src="/ML.png" alt="ML logo" style={{height: "100px"}} />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
                     
-                </Nav>
-                 
-                </Navbar.Collapse>
-                <CartDropdown />
-                <CartTest />
-            </Container>
-        </Navbar>
+                    <Nav className="ms-auto fs-3">
+                        <Nav.Link href="/" className="mx-2" >Weavings</Nav.Link>
+                        {/* <Nav.Link href="/about" className="mx-2" >About</Nav.Link> */}
+                        <Nav.Link href="/contact" className="mx-2" >Contact</Nav.Link>
+                        
+                    </Nav>
+                    
+                    </Navbar.Collapse>
+                    
+
+                    <CartDropdown handleOpen={handleOpen}  />
+                    
+                    <CartTest />
+                </Container>
+            </Navbar>
+            <Offcanvas show={show} onHide={handleClose}>
+                <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+                </Offcanvas.Header>
+                <Offcanvas.Body>
+                Some text as placeholder. In real life you can have the elements you
+                have chosen. Like, text, images, lists, etc.
+                </Offcanvas.Body>
+            </Offcanvas>
+
+        </div>
     )
 }
 
