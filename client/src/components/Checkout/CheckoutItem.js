b1e41826b04c5a72a5147adcd10c1f2c7cccc2f9
Andy Patari <andy.patari@gmail.com>
2024-05-19 12:51:56 -0400

    cart items displayed on checkout, no styling

--- client/src/components/Checkout/CheckoutItem.js
+++ client/src/components/Checkout/CheckoutItem.js
@@ -1,9 +1,57 @@
 import React from "react";
+import { Row, Col, Button,  } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
 export default function CheckoutItem({ piece, setCart }) {
+
+    const handleDelete = () => {
+    
+        fetch('/api/cart', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "same-origin",
+            body: JSON.stringify({"id": parseInt(piece.id)})
+        })
+        .then(res => res.json())
+        .then(data => {
+            setCart(data)
+            
+        })
+        
+    }
+
     return (
-        <div>
-            <p>{piece.name}</p>
+        <div >
+            <Row >
+                <Col md={8} className="bg-secondary bg-opacity-25 m-2 rounded p-2" >
+                    <Row>
+
+                        <Col className="fs-5"sm={5} >
+                            {piece.name}
+                        </Col>
+                        <Col>
+                            <img className="mx-auto" src={`/api/pics/${piece.image_url}`} alt="Piece" style={{width: '5rem', cursor:'pointer'}} />
+                        </Col>
+                        <Col className="" >
+                            <div className="text-end fs-4">
+                                ${piece.price}
+
+                            </div>
+                            
+                        </Col>
+                        <Col className="d-flex">
+                            <Button onClick={handleDelete} className="ms-auto rounded btn-secondary delbtn" >
+                                <FontAwesomeIcon icon={faTrash} />
+                            </Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            
+ 
         </div>
     )
 }
