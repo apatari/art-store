dd0bae77fc51ef526d132c716d590542adfa702c
Andy Patari <andy.patari@gmail.com>
2024-05-20 12:38:22 -0400

    create user info form structure

--- client/src/components/Checkout/GuestCO.js
+++ client/src/components/Checkout/GuestCO.js
@@ -1,9 +1,48 @@
 import React from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
 
 export default function GuestCO() {
     return (
         <div className="my-3 mx-5 p-3 bg-light rounded">
-            Guest CO
+            <Form>
+                <Row className="mb-3" >
+                    <Form.Group as={Col} controlId="formGridEmail" md={6} >
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>   
+                </Row>
+
+                <Form.Group className="mb-3" controlId="formGridAddress1">
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control placeholder="1234 Main St" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formGridAddress2">
+                    <Form.Label>Address 2</Form.Label>
+                    <Form.Control placeholder="Apartment, studio, or floor" />
+                </Form.Group>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} controlId="formGridCity">
+                    <Form.Label>City</Form.Label>
+                    <Form.Control />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridState">
+                    <Form.Label>State (2 letter abbr.)</Form.Label>
+                    <Form.Control />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="formGridZip">
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control />
+                    </Form.Group>
+                </Row>
+
+                <Button variant="primary" type="submit">
+                    Next
+                </Button>
+            </Form>
         </div>
     )
 }
