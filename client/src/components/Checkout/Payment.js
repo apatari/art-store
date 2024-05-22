162f9ff7902e0a2ecdfee4ea70c081eab2c7fd67
Andy Patari <andy.patari@gmail.com>
2024-05-22 09:06:37 -0400

    create payment intent and render payment element

--- client/src/components/Checkout/Payment.js
+++ client/src/components/Checkout/Payment.js
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+
+import CheckoutForm from "./CheckoutForm";
+
+// stripe
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import { CartContext } from "../App";
+
+const stripe_key = process.env.REACT_APP_STRIPE_KEY
+const stripePromise = loadStripe(stripe_key);
+
 
 export default function Payment({userInfo}) {
+
+    const [cart] = useContext(CartContext)
+
+    const [clientSecret, setClientSecret] = useState("")
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("/api/create-payment-intent", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "cart": cart, "userInfo": userInfo }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret))
+
+    }, [cart, userInfo])
+
+    const appearance = {
+        theme: 'stripe',
+      };
+      const options = {
+        clientSecret,
+        appearance,
+      };
+
     return (
         <div className="my-3 mx-5 p-3 bg-light rounded">
-            {userInfo.email}
+            <h3>Please enter payment information</h3>
+            {clientSecret && (
+            <Elements options={options} stripe={stripePromise}>
+              <CheckoutForm />
+            </Elements>
+          )} 
         </div>
     )
 }
