7d9d07c7b89b2d11e5f911c519d49134cbb932d8
Andy Patari <andy.patari@gmail.com>
2024-06-03 09:44:55 -0400

    move login routes to separate file

--- server/app.py
+++ server/app.py
@@ -19,40 +19,15 @@ from models import Seller, Piece, Order
 #import routes from separate files
 import routes.stripeRoutes
 
+# import flask_restful routes
+from routes.loginRoutes import Login, Logout, CheckSession
+
 ALLOWED_EXTENSIONS = {'png', 'jpg', "jpeg"}
 
 
 # Views go here!
 
-class Login(Resource):
 
-    def post(self):
-        username = request.get_json()['username']
-        password = request.get_json()['password']
-
-        user = Seller.query.filter_by(username=username).first()
-
-        if user and user.authenticate(password):
-            session['user_id'] = user.id
-            response_body = {
-                "id": user.id,
-                "username": user.username,
-            }
-            return response_body, 200
-        else:
-            return {"errors": ["Invalid username and/or password"]}, 401
-        
-class Logout(Resource):
-    
-    def delete(self):
-
-        user = Seller.query.filter_by(id = session.get('user_id')).first()
-
-        if user:
-            session["user_id"] = None
-            return {}, 200
-        else:
-            return{"errors": "Error: cannot log out, you are not logged in"}, 401
         
 class PieceIndex(Resource):
 
@@ -128,20 +103,7 @@ class PieceByID(Resource):
         else:
             return {"error": "Piece not found"}, 404
         
-class CheckSession(Resource):
 
-    def get(self):
-
-        user = Seller.query.filter_by(id = session.get('user_id')).first()
-
-        if user:
-            response_body = {
-                "id": user.id,
-                "username": user.username,
-            }
-            return response_body, 200
-        else:
-            return {"errors": "User not logged in"}, 401
         
 class CheckCart(Resource):
     def get(self):
---
+++ server/routes/loginRoutes.py
@@ -0,0 +1,57 @@
+
+
+# Remote library imports
+from flask import request, session
+from flask_restful import Resource
+
+
+# Local imports
+
+# Add your model imports
+from models import Seller, Piece, Order
+
+
+class Login(Resource):
+
+    def post(self):
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+
+        user = Seller.query.filter_by(username=username).first()
+
+        if user and user.authenticate(password):
+            session['user_id'] = user.id
+            response_body = {
+                "id": user.id,
+                "username": user.username,
+            }
+            return response_body, 200
+        else:
+            return {"errors": ["Invalid username and/or password"]}, 401
+        
+class Logout(Resource):
+    
+    def delete(self):
+
+        user = Seller.query.filter_by(id = session.get('user_id')).first()
+
+        if user:
+            session["user_id"] = None
+            return {}, 200
+        else:
+            return{"errors": "Error: cannot log out, you are not logged in"}, 401
+        
+class CheckSession(Resource):
+
+    def get(self):
+
+        user = Seller.query.filter_by(id = session.get('user_id')).first()
+
+        if user:
+            response_body = {
+                "id": user.id,
+                "username": user.username,
+            }
+            return response_body, 200
+        else:
+            return {"errors": "User not logged in"}, 401
