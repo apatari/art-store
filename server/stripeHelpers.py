2c53f0665fdcee4aebfca592b9ed09dbfea31c60
Andy Patari <andy.patari@gmail.com>
2024-06-27 09:40:55 -0400

    re-format address spacing

--- server/stripeHelpers.py
+++ server/stripeHelpers.py
@@ -5,15 +5,15 @@ def makeTextConfirmation(order):
     address=""
     if order.address2:
         address=f"""
-{order.address}
-{order.address2 or ""}
-{order.city + ", " + order.state + ", " + order.zip}"""
+    {order.address}
+    {order.address2 or ""}
+    {order.city + ", " + order.state + ", " + order.zip}"""
     else:
         address=f"""
-{order.address}
-{order.city + ", " + order.state + ", " + order.zip}"""
+    {order.address}
+    {order.city + ", " + order.state + ", " + order.zip}"""
 
-    with open(f'/Users/andypatari/Development/code/post-projects/art-store/server/confirmations/{filename}', 'w') as file:
+    with open(f'/Users/andypatari/Development/code/post-projects/art-store/server/confirmationsByText/{filename}', 'w') as file:
         file.write(f"""
 Email: {order.customer_email}
 Thank you for your purchase!
